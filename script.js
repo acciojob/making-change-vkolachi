3ff082b0e7644f9909f505b500e879fc208eb91f
vkolachi <36785379+vkolachi@users.noreply.github.com>
2023-11-29 17:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const makeChange = (c) => {
-  // your name here
+  let units = {
+    q:25,
+    d:10,
+    n:5,  
+    p:1
+}    
+let ans={};
+for(let i in units){
+  let currentunits=units[i];
+  ans[i]=parseInt(c/currentunits);
+  c=c%currentunits;
+} 
+	return ans; 
 };
 
 // Do not the change the code below
